714ebf0f7711b106d1d7cb302ec2704c73c69aaa
David Smits <david.j.smits@gmail.com>
2018-08-16 06:43:16 -0500

    Reg prep

--- RegisterLaunch.sh
+++ RegisterLaunch.sh
@@ -1,2 +1,4 @@
-cd /home/farmer4/Register
-python register.py
+cd /home/farmer3/Register
+CURRENT_DATE="$(date +%Y-%m-%d)"
+echo $CURRENT_DATE
+python register.py >> /home/farmer3/Register/logs/$CURRENT_DATE
